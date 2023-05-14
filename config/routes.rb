38690bfe9ee96b446d55c7cb6b349d962fd65e5c
ridwanullahi <ridwanullahiajayi01@gmail.com.com>
2023-05-14 14:06:08 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-      resources :transactions
-      resources :users
-      root to: "users#index"
-    end
-
-  devise_for :users
-
-  root 'user#show'
-
-  get 'deposit' => 'user#deposit', :as => 'deposit'
-  get 'withdraw' => 'user#withdraw', :as => 'withdraw'
-  get 'new_user_referral' => 'user#referral', :as => 'new_user_referral'
+  root 'user#index'
 end
